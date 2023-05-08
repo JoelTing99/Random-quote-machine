48420df2a1f5a19e210d857dd3bc11200bcf9a9b
Joe <93369182+JoelTing99@users.noreply.github.com>
2023-05-08 19:31:13 +0800

    completed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import {Button, Container, Row, Col, Card} from "react-bootstrap";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
 import quotes from "./quotes.json";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 const colors = [
   '#16a085',
@@ -26,32 +29,75 @@ class App extends React.Component {
 
     this.state = {
       currentQuote: "",
+      currentAuthor: "",
       currentColor: "#16a085",
     }
+
+    this.handleNewQuote = this.handleNewQuote.bind(this);
   }
+
+  handleNewQuote = () => {
+
+    let quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+    this.setState({
+      currentQuote: quote.quote,
+      currentAuthor: quote.author,
+      currentColor: this.randomColor(),
+    });
+  }
+
+  randomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)]
+  }
+
+  componentDidMount() {
+    this.handleNewQuote();
+  }
+
 
   render() {
     return (
-      <div className='position-relative' id='quote-box' style={{height: "100vh", color: this.state.currentColor, backgroundColor: this.state.currentColor}}>
-        <Card className='text-center position-absolute top-50 start-50 translate-middle' style={{width: "30rem"}}>
+      <div className='d-flex flex-column justify-content-center align-items-center' id='quote-box' style={{
+         height: "100vh", 
+         color: this.state.currentColor, 
+         backgroundColor: this.state.currentColor,
+         transition: "all 1s ease",  }}>
+        <Card className='text-center p-4' style={{ width: "35rem" }}>
           <Container>
             <Row className='mt-5 mb-4 px-5'>
               <Col>
-                <Card.Title id="text">" Life isn’t about getting and having, it’s about giving and being.</Card.Title>
+                <Card.Title id="text" style={{
+                  transition: "opacity 1s ease",
+                }}><FontAwesomeIcon icon={faQuoteLeft} /> {this.state.currentQuote}</Card.Title>
               </Col>
             </Row>
             <Row>
               <Col className='text-end'>
-                <Card.Subtitle id="author">- Kevin Kruse</Card.Subtitle>
+                <Card.Subtitle id="author">- {this.state.currentAuthor}</Card.Subtitle>
               </Col>
             </Row>
-            <Row className='my-4'>
+            <Row className='m-4'>
+              <Col md={1} className='me-auto'>
+                <a className='btn' id="tweet-quote" target="_top" 
+                style={{ 
+                  backgroundColor: this.state.currentColor,
+                  transition: "all 1s ease", 
+                }} href={"https://twitter.com/intent/tweet?hashtags=quotes&text=" + encodeURIComponent('"' + this.state.currentQuote + '" ' + this.state.currentAuthor)}>
+                  <FontAwesomeIcon icon={faTwitter} size='gl'/>
+                </a>
+              </Col>
               <Col md={4} className='ms-auto'>
-                <Button id="new-quote" style={{backgroundColor: this.state.currentColor}}>New Quote</Button>
+                <Button id="new-quote" variant="light" style={{
+                   backgroundColor: this.state.currentColor,
+                   transition: "all 1s ease",  
+                  }} onClick={this.handleNewQuote}>New Quote
+                </Button>
               </Col>
             </Row>
           </Container>
         </Card>
+        <a className='text-light my-2 text-decoration-none' target="_blank" href='https://github.com/JoelTing99'>by JoelTing</a>
       </div>
     );
   }
